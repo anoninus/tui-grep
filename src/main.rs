f0a8f76ed12a4516db312071368a052a43bd5372
anoninus <anoninuxt@gmail.com>
2026-03-06 17:28:05 +0530

    added working mixed flag support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "rgrep",
+    about = "The fzf line implementation for grep in my own style"
+)]
+struct Cli {
+    #[arg(short, long)]
+    recursive: bool,
+
+    #[arg(short, long)]
+    ignore_case: bool,
+
+    // ignore evrything after the flags
+    #[arg(last = true, allow_hyphen_values = true)]
+    pattern: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    if args.recursive {
+        println!("Trigger recursive tui");
+    }
+    if args.ignore_case {
+        println!("Trigger ignore_case tui");
+    }
 }
